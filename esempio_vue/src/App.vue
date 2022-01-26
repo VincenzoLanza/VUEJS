884ec6a80a5c21648f6a3b3cfd9f6cca8507ba82
VincenzoLanza <vincenzo.lanza@aidia.it>
2022-01-26 17:54:01 +0100

    esempio vue

--- esempio_vue/src/App.vue
+++ esempio_vue/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <app-header></app-header>
+     <ninjas></ninjas>
+     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+export default { 
+  components:{
+      "app-header":Header,
+      "app-footer":Footer,
+    Ninjas
+  },
+  data(){
+     return{
+       title:"titolo"
+     }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
